146ba8a6fd39e959e88d82c72d36c94c613b02ca
Vlad Poncea <vlad0poncea@gmail.com>
2021-08-17 14:11:40 +0300

    first commit

---
+++ StickXSport/StickXSport/StickXSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickXSportApp.swift
+//  StickXSport
+//
+//  Created by Vlad Poncea on 17.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StickXSportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
